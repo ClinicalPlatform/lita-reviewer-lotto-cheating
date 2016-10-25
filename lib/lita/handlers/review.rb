98b0e7df9537c0ea23da7c37f535c014033bdffe
hyone <hyone.development@gmail.com>
2016-10-25 16:13:30 +0900

    Implement feature to get pullrequests from github API without specifying argument

--- lib/lita/handlers/review.rb
+++ lib/lita/handlers/review.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 require 'octokit'
@@ -6,69 +8,122 @@ require 'uri'
 module Lita
   module Handlers
     class Review < Handler
-      config :github_access_token
-      # expiration time (second) of a redis entry which records pullrequest reviewers,
-      # which is used to calculate review count for specific user when selecting reviewers
-      config :pullrequest_log_expiration
+      REDIS_PULLREQUESTS_KEY         = 'pullrequests'
+      REDIS_ORDERED_PULLREQUESTS_KEY = 'pullrequests_ordered'
+      REDIS_USERS_KEY                = 'users'
 
-      route /review\s+(#{ URI.regexp })\b/,
-            :lookup_reviewers,
+      config :github_access_token
+      # repositories to review
+      config :repositories
+      # duration time (second) from now, which is used to calculate review count
+      # for specific user when selecting reviewers
+      config :reviewer_count_duration
+      # labels that is set to pullrequests for review
+      config :pr_labels
+      # default room(channel) or user to which this handler sends messages
+      config :default_chat_target
+
+      on :connected, :assign_reviewers_to_all
+
+      route /reviewer\s+(#{ URI.regexp })\b/,
+            :assign_reviewers_from_chat,
             command: true,
             help: {
-              'review GITHUB_PR_URL' => t('help.description')
+              'reviewer GITHUB_PR_URL' => t('help.description')
             }
 
-      def lookup_reviewers(response)
-        @response = response
-        uri = @response.matches[0][0]
-
-        begin
-          prepare_repository_client(uri)
-        rescue Error, Octokit::Error, URI::InvalidURIError => e
-          return error(e.message)
-        end
-
-        # do not select reviewers when it has already done for the PR.
-        pr_key = "pullrequests:#{URI.parse(@pr.html_url).path}"
-        return response.reply t('reply.already_assigned', uri: @pr.html_url) \
-          if redis.exists(pr_key)
-
-        reviewers = select_reviewers
-
-        begin
-          write_pr_comment(reviewers)
-        rescue Octokit::Error => e
-          return error(t('error.comment_failure', text: e.message))
-        end
-
-        response.reply t(
-          'reply.selected_reviewers.chat',
-          reviewers: reviewers.map { |s| "@#{s}" }.join(' and '),
-          uri: @pr.html_url,
+      def initialize(*args)
+        super
+        @gh_client = Octokit::Client.new(
+          access_token: config.github_access_token
         )
+      end
 
-        save_reviwers(
-          pr_key,
-          reviewers,
-          expiration: config.pullrequest_log_expiration || 30 * 24 * 60 * 60  # default 1 month
-        )
+      def assign_reviewers_to_all(_payload)
+        return logger.info(
+          %('config.handlers.review.repositories' is not set. exit.)
+        ) unless config.repositories
+
+        prs = pull_requests(config.repositories)
+        logger.debug("Found pullrequests: #{prs.map { |pr| URI.parse(pr.html_url).path }}")
+
+        prs.each do |pr|
+          assign_reviewers(pr)
+        end
+      end
+
+      def assign_reviewers_from_chat(response)
+        begin
+          assign_reviewers_from_uri(response.matches[0][0])
+        rescue Error, Octokit::Error => e
+          send_error(e.message, target: response)
+        end
+      end
+
+      def assign_reviewers_from_uri(uri)
+        repo_uri, pr_uri, pr_number = parse_uri(uri)
+        repo = Octokit::Repository.from_url("#{repo_uri}")
+        pr   = @gh_client.pull_request(repo, pr_number)
+
+        assign_reviewers(pr)
       end
 
       private
 
-      def prepare_repository_client(uri)
-        repo_uri, pr_uri, pr_number = parse_uri(uri)
-        @gh_client = Octokit::Client.new(access_token: config.github_access_token)
-        @repo      = Octokit::Repository.from_url("#{repo_uri}")
-        @pr        = @gh_client.pull_request(@repo, pr_number)
+      def assign_reviewers(pr)
+        key = pr_key(pr)
+        return logger.info("#{pr.html_url} is already assigned") \
+          if redis.sismember(REDIS_PULLREQUESTS_KEY, pr.id)
+
+        reviewers = select_reviewers(*config.reviewer_count_duration)
+        logger.debug("Select #{reviewers} on #{pr.html_url}")
+
+        begin
+          write_pr_comment(pr, reviewers)
+        rescue Octokit::Error => e
+          return logger.error("Failed to write comment to the pullrequest page: #{e.message}")
+        end
+
+        target = Source.new(room: '#general')
+        robot.send_message(target, t(
+          'message.assigned_reviewers.chat',
+          reviewers: reviewers.map { |s| "@#{s}" }.join(', '),
+          uri: pr.html_url,
+        ) )
+
+        save_to_redis(key: key, pr_id: pr.id, reviewers: reviewers)
+
+        logger.info("Assigned #{reviewers.join(', ')} as reviewers for #{pr.html_url}")
+      end
+
+      def pull_requests(repositories)
+        repositories.map do |repository|
+          repo_name =
+            case repository
+            when String then repository
+            when Hash   then repository[:name]
+            end
+          options =
+            case repository
+            when Hash   then { labels: repository[:labels].join(',') }
+            else {}
+            end
+
+          repo   = Octokit::Repository.new(repo_name)
+          pulls  = @gh_client.pulls(repo_name)
+          issues = @gh_client.issues(repo_name, options).select { |i| i.pull_request }
+
+          issues.map do |issue|
+            pulls.find { |pull| pull.number == issue.number }
+          end.reject(&:nil?)
+        end.flatten
       end
 
       def parse_uri(uri)
         pr_uri =
           URI.parse(uri).tap do |u|
             raise Error.new(t('error.invalid_uri', uri: u)) \
-              unless u.scheme =~ /^https?$/ \
-                and  u.host == 'github.com'
+              unless u.scheme =~ /^https?$/ and u.host == 'github.com'
           end
 
         _, repo_path, pr_number = pr_uri.path.match(%r|^((?:/[^/]+){2})/pull/(\d+).*$|).to_a
@@ -79,20 +134,33 @@ module Lita
         [repo_uri, pr_uri, pr_number]
       end
 
-      def select_reviewers
+      def select_reviewers_by_working_days(members)
+        members.select do |member|
+          working_days = redis.smembers("#{REDIS_USERS_KEY}:#{member}:working_days")
+                              .map(&:to_i)
+          working_days.include?(DateTime.now.cwday)
+        end
+      end
+
+      def select_reviewers(duration = 30 * 24 * 60 * 60)
         # calculate count of reviewed for each user
+        now   = Time.now.to_i
+        start = now - duration
         user_points = \
-          redis.keys('pullrequests:*')
+          redis.zrangebyscore(REDIS_ORDERED_PULLREQUESTS_KEY, start, now)
                .map { |k| redis.smembers(k) }
                .flatten
                .each_with_object({}) do |user, hash|
                  hash[user] = hash.fetch(user, 0) + 1
                end
-        # p user_points
+        logger.debug("Current reviewer counts: #{user_points}")
 
-        members = redis.smembers('users')
+        members = select_reviewers_by_working_days(
+          redis.smembers(REDIS_USERS_KEY)
+        )
+
         siniors, juniors = members.partition do |user|
-          level = redis.hget("users:#{user}", :level).to_i
+          level = redis.get("#{REDIS_USERS_KEY}:#{user}:level").to_i
           level > 1
         end
         reviewers = [siniors, juniors].map do |m|
@@ -103,23 +171,49 @@ module Lita
       end
 
       # write comment to tell assignment of the reviewers to github pullrequest page
-      def write_pr_comment(reviewers)
-        @gh_client.add_comment(
-          @repo,
-          @pr.number,
-          t('reply.selected_reviewers.comment', reviewers: reviewers.join(' and '))
+      def write_pr_comment(pr, reviewers)
+        reviewers_text = reviewers.join(', ')
+        text = t('message.assigned_reviewers.comment',
+                 reviewers: reviewers_text)
+        repo = pr.head.repo.full_name
+
+        @gh_client.add_comment(repo, pr.number, text)
+        logger.debug("Wrote a comment on #{pr.html_url}")
+
+        # display in status check
+        @gh_client.create_status(
+          repo, pr.head.sha, :pending,
+          context: t('application_name'),
+          description: text
         )
+        logger.debug("Set status on #{pr.html_url}")
       end
 
-      def save_reviwers(pr_key, reviewers, expiration:)
-        redis.sadd(pr_key, reviewers)
-        # a review record expires for `expiration`, which is used for review count calculation
-        # redis.expire(pr_key, 30 * 24 * 60 * 60)
-        redis.expire(pr_key, expiration)
+      def save_to_redis(key:, pr_id:, reviewers:)
+        # To detect whether or not it was reviewd in the future
+        redis.sadd(REDIS_PULLREQUESTS_KEY, pr_id)
+        # a review record which is used for review count calculation
+        redis.zadd(REDIS_ORDERED_PULLREQUESTS_KEY, Time.now.to_i, key)
+        # log to assing reviewers
+        redis.sadd(key, reviewers)
       end
 
-      def error(text)
-        @response&.reply("Error: #{text}")
+      def pr_key(pr)
+        "#{REDIS_PULLREQUESTS_KEY}:#{URI.parse(pr.html_url).path}"
+      end
+
+      def send_error(text, target:)
+        text = "Error: #{text}"
+        case target
+        when Lita::Source
+          robot.send_message(target, text)
+        when Lita::Response
+          target.reply(text)
+        end
+      end
+
+      def logger
+        Lita.logger
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/review_spec.rb
+++ spec/lita/handlers/review_spec.rb
@@ -2,20 +2,22 @@ require "spec_helper"
 
 describe Lita::Handlers::Review, lita_handler: true do
   describe 'route' do
-    context 'with valid argument' do
-      it { is_expected.to route_command('review http://www.example.com/').to(:lookup_reviewers) }
-    end
+    context 'review' do
+      context 'with valid argument' do
+        it { is_expected.to route_command('reviewer http://www.example.com/').to(:lookup_reviewers) }
+      end
 
-    context 'with invalid argument' do
-      it { is_expected.not_to route_command('review hoge').to(:lookup_reviewers) }
-    end
+      context 'with invalid argument' do
+        it { is_expected.not_to route_command('reviewer hoge').to(:lookup_reviewers) }
+      end
 
-    context 'without arguments' do
-      it { is_expected.not_to route_command('review').to(:lookup_reviewers) }
-    end
+      context 'without arguments' do
+        it { is_expected.not_to route_command('reviewer').to(:lookup_reviewers) }
+      end
 
-    context 'when not command' do
-      it { is_expected.not_to route('review http://www.example.com/').to(:lookup_reviewers) }
+      context 'when not command' do
+        it { is_expected.not_to route('reviewer http://www.example.com/').to(:lookup_reviewers) }
+      end
     end
   end
 
@@ -23,19 +25,19 @@ describe Lita::Handlers::Review, lita_handler: true do
     subject { replies.last }
 
     context 'with non http(s) URL' do
-      before { send_command('review ftp://github.com/hyone/test1/pull/3') }
+      before { send_command('reviewer ftp://github.com/hyone/test1/pull/3') }
       it { is_expected.to eq 'Error: "ftp://github.com/hyone/test1/pull/3" is not github pullrequest URL.' }
     end
 
     context 'with non pullrequest URL' do
-      before { send_command('review https://github.com/hyone/test1/pul') }
+      before { send_command('reviewer https://github.com/hyone/test1/pul') }
       it { is_expected.to eq 'Error: "https://github.com/hyone/test1/pul" is not github pullrequest URL.' }
     end
 
     context 'with non existing pullrequest' do
       before do
         VCR.use_cassette('hyone/test1/pull/9999') do
-          send_command('review https://github.com/hyone/test1/pull/9999')
+          send_command('reviewer https://github.com/hyone/test1/pull/9999')
         end
       end
 
@@ -52,11 +54,21 @@ describe Lita::Handlers::Review, lita_handler: true do
           receive(:select_reviewers).and_return(['foo', 'bar'])
 
         VCR.use_cassette('hyone/test1/pull/3') do
-          send_command('review https://github.com/hyone/test1/pull/3')
+          send_command('reviewer https://github.com/hyone/test1/pull/3')
         end
       end
 
-      it { is_expected.to eq '@foo and @bar are selected as the reviewers for https://github.com/hyone/test1/pull/3!!' }
+      it { is_expected.to eq '@foo and @bar are assigned as the reviewers for https://github.com/hyone/test1/pull/3!!' }
+
+      context 'and send same reviewer command again' do
+        before do
+          VCR.use_cassette('hyone/test1/pull/3') do
+            send_command('reviewer https://github.com/hyone/test1/pull/3')
+          end
+        end
+
+        it { is_expected.to eq 'https://github.com/hyone/test1/pull/3 has already assigned to reviewers.' }
+      end
     end
   end
 end
