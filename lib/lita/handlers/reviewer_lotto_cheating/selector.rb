34bf8325d299dfefcb105967e87d7ff5124ee5eb
hyone <hyone.development@gmail.com>
2017-01-18 19:24:28 +0900

    Choice total 2 reviewers when an either grouup has no user

--- lib/lita/handlers/reviewer_lotto_cheating/selector.rb
+++ lib/lita/handlers/reviewer_lotto_cheating/selector.rb
@@ -21,30 +21,37 @@ module Lita::Handlers::ReviewerLottoCheating
 
       def select(users, reviewed_counts)
         siniors, juniors = users.partition { |user| user.level > 1 }
+        hasEmptyGroup = [siniors, juniors].any?(&:empty?)
 
-        [siniors, juniors].map do |group|
-          user_points = group.map do |u|
-            num_working_days = u.working_days.size.nonzero? || 1
-            # reviewed point is multiplied by 100 and rounded off
-            point = (reviewed_counts.fetch(u.name, 0).to_f / num_working_days * 100).round
-            [u, point]
-          end.to_h
-
-          max_point = user_points.max_by {|t| t[1] }&.at(1)&.nonzero? || 100
-          sorted = group.sort_by do |u|
-            # final point consists of the ratio of 'reviewed point' to 'random point' are 8 : 2
-            reviewed_point = user_points.fetch(u, 0) * 8
-            random_point   = (rand(max_point) + 1) * 2
-            point          = reviewed_point + random_point
-            Lita.logger.debug(
-              "[#{u.name} - lv.#{u.level}] review: #{reviewed_point}, " \
-              "random: #{random_point}: total: #{point}"
-            )
-            point
+        [siniors, juniors]
+          .map do |group|
+            sorted = sort_users_by_point_and_lotto(group, reviewed_counts)
+            sorted.take(hasEmptyGroup ? 2 : 1)
           end
-          sorted.first
+          .flatten
+          .compact
+      end
+
+      def sort_users_by_point_and_lotto(users, reviewed_counts)
+        user_points = users.map do |u|
+          num_working_days = u.working_days.size.nonzero? || 1
+          # reviewed point is multiplied by 100 and rounded off
+          point = (reviewed_counts.fetch(u.name, 0).to_f / num_working_days * 100).round
+          [u, point]
+        end.to_h
+
+        max_point = user_points.max_by { |t| t[1] }&.at(1)&.nonzero? || 100
+        users.sort_by do |u|
+          # final point consists of the ratio of 'reviewed point' to 'random point' are 8 : 2
+          reviewed_point = user_points.fetch(u, 0) * 8
+          random_point   = (rand(max_point) + 1) * 2
+          point          = reviewed_point + random_point
+          Lita.logger.debug(
+            "[#{u.name} - lv.#{u.level}] review: #{reviewed_point}, " \
+            "random: #{random_point}: total: #{point}"
+          )
+          point
         end
-        .compact
       end
     end
   end
