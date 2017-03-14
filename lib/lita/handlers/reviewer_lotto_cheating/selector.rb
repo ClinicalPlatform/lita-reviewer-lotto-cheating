64d642121f588418f7ec187edb571658f50623f3
hyone <hyone.development@gmail.com>
2017-03-14 15:33:28 +0900

    Support to configure the randomness weight in the reviewers selection factors

--- lib/lita/handlers/reviewer_lotto_cheating/selector.rb
+++ lib/lita/handlers/reviewer_lotto_cheating/selector.rb
@@ -7,31 +7,31 @@ require 'lita/handlers/reviewer_lotto_cheating/error'
 module Lita::Handlers::ReviewerLottoCheating
   class Selector
     class << self
-      def call(users:, duration:)
+      def call(users:, duration:, random_weight: nil)
         raise Error.new('no user as reviewer candidation') if users.empty?
 
         user_points = Pullrequest.calc_review_counts(duration: duration)
         Lita.logger.debug("Current reviewer counts: #{user_points}")
 
-        select(users, user_points)
+        select(users, user_points, random_weight: random_weight)
       end
 
       private
 
-      def select(users, reviewed_counts)
+      def select(users, reviewed_counts, random_weight: nil)
         siniors, juniors = users.partition { |user| user.level > 1 }
         hasEmptyGroup = [siniors, juniors].any?(&:empty?)
 
         [siniors, juniors]
           .map do |group|
-            sorted = sort_users_by_point_and_lotto(group, reviewed_counts)
+            sorted = sort_users_by_point_and_lotto(group, reviewed_counts, random_weight: random_weight)
             sorted.take(hasEmptyGroup ? 2 : 1)
           end
           .flatten
           .compact
       end
 
-      def sort_users_by_point_and_lotto(users, reviewed_counts)
+      def sort_users_by_point_and_lotto(users, reviewed_counts, random_weight: nil)
         user_points = users.map do |u|
           num_working_days = u.working_days.size.nonzero? || 1
           # reviewed point is multiplied by 100 and rounded off
@@ -39,14 +39,18 @@ module Lita::Handlers::ReviewerLottoCheating
           [u, point]
         end.to_h
 
+        random_weight = random_weight.try! { self % 101 } || 20    # default 20
+        review_weight = 100 - random_weight
+        Lita.logger.debug("Sort reviewrs (reviewed : random is #{review_weight} : #{random_weight})")
+
         max_point = user_points.max_by { |t| t[1] }&.at(1)&.nonzero? || 100
+
         users.sort_by do |u|
-          # final point consists of the ratio of 'reviewed point' to 'random point' are 8 : 2
-          reviewed_point = user_points.fetch(u, 0) * 8
-          random_point   = (rand(max_point) + 1) * 2
-          point          = reviewed_point + random_point
+          review_point = user_points.fetch(u, 0) * review_weight
+          random_point = (rand(max_point) + 1)   * random_weight
+          point        = review_point + random_point
           Lita.logger.debug(
-            "[#{u.name} - lv.#{u.level}] review: #{reviewed_point}, " \
+            "[#{u.name} - lv.#{u.level}] review: #{review_point}, " \
             "random: #{random_point}: total: #{point}"
           )
           point
